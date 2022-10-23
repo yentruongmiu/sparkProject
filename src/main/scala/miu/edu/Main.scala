9408524644babaf2221a8f3aac132a0d56737286
Yen Truong <truthblue82@gmail.com>
2022-10-23 10:09:24 -0500

    optimize code

--- src/main/scala/miu/edu/Main.scala
+++ src/main/scala/miu/edu/Main.scala
@@ -1,7 +1,10 @@
 package miu.edu
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.connector.catalog.TableChange.ColumnPosition.first
+import org.apache.spark.sql.functions.monotonicallyIncreasingId
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 /**
  * Data: Gapminder data
@@ -15,7 +18,7 @@ object Main {
   val MIN_KEYS = 4
 
   def doubleRound(d: Double): Double = {
-    val res = BigDecimal(d).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val res = BigDecimal(d).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
     res
   }
 
@@ -43,6 +46,13 @@ object Main {
     val meanAndVariance = mean.mapValues(v => v._1).join(variance)
     meanAndVariance
   }
+
+  def errorPercentage(actual:Double, estimate:Double): Double = {
+    val result = Math.abs(actual - estimate)*100/actual
+    //println("actual:"+actual + ", estimate:"+estimate + ", result:"+result)
+    doubleRound(result)
+  }
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
       .appName("BigData - Spark Project")
@@ -107,6 +117,40 @@ object Main {
     average.map(v => (v._1, doubleRound(v._2._1), doubleRound(v._2._2)))
       .toDF("Category", "Mean", "Variance")
       .show()
+
+    //Determine the absolute error percentage for each of the values being estimated: abs(actual – estimate)*100/actual
+    val dfAll = meanAndVariance.sortByKey().join(average)
+      .map(x => (x._1, errorPercentage(x._2._1._1, x._2._2._1), errorPercentage(x._2._1._2, x._2._2._2)))
+      .sortBy(x => x._1, true)
+      .toDF("Continent", "Mean", "Variance") //"Continent", "Mean", "Variance"
+      //.show()
+    println("======PERCENTS======")
+    //percentsAll.foreach(println)
+    dfAll.show()
+
+
+//    val res = dfAll.withColumn("Continent", monotonicallyIncreasingId).groupBy("Continent").pivot("Mean")
+//      .agg(dfAll.first()).show()
+
+    //val newSchema = StructType(dfAll.select(0).first().getAs[Seq[String]](0).map(z => StructField(z, StringType)))
+//    val new_schema = StructType(df1.select(collect_list("Column")).first().getAs[Seq[String]](0).map(z => StructField(z, StringType)))
+//    val new_values = sc.parallelize(Seq(Row.fromSeq(df.select(collect_list("Value")).first().getAs[Seq[String]](0))))
+//    sqlContext.createDataFrame(new_values, new_schema).show(false)
+
+      //(Africa, (2.01, 12.13))
+      //(Asia, (0.60, 8.62))
+    //(Americas, (0.51, 8.04))
+    //(Oceania, (0.98, 11.12))
+    //(Europe, (0.51, 17.17))
+    //=> (Percentage, Africa-M, Africa-V, Asia-M, Asia-V, Americas-M, Americas-V, Oceania-M, Oceania-V, Europe-M, Europe-V)
+    //=> (25,  (2.01, 12.13), (0.60, 8.62), (0.51, 8.04), (0.98, 11.12), (0.51, 17.17)
+
+
+//    (Africa,48.86533012820508,49.065439087995095,83.72634723273455,100.6580062781063)
+//    (Asia,60.064903232323175,61.199140705446766,140.76710837747206,131.42413997450512)
+//    (Americas,64.65873666666667,64.52482573703185,87.33066977992195,85.08237005427381)
+//    (Oceania,74.32620833333333,73.10676174324293,14.406663302536224,16.540266747112838)
+//    (Europe,71.90368611111106,71.05560947014297,29.519421096185418,45.3717385773788)
 
     //Step 7: Draw a graph with x-axis percentage
   }
